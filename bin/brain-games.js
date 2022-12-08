f88e32f12575e94fb40ed2e7c88d823e5612b7b2
qhp21 <veber_33@bk.ru>
2022-12-08 11:56:29 +0300

    stepThreeDone

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-const name = readlineSync.question('Your answer: ');
-console.log('Welcome to the Brain Games!');
+import greeting from '../src/cli.js';
+greeting();
