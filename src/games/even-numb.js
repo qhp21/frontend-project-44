d857c91e2e6e40bfb726709fd5dee8a37e52043b
Михаил Вебер <veber_22@bk.ru>
2023-05-18 22:50:07 +0300

    runGame for even

--- src/games/even-numb.js
+++ src/games/even-numb.js
@@ -1,32 +1,21 @@
 import readlineSync from 'readline-sync';
+import { runGame } from '../index.js';
+
+const isEven = (num) => num % 2 === 0;
+
+const generateQuestion = () => {
+  const number = Math.round(Math.random() * 100);
+  const question = `Question: ${number}`;
+  const answer = isEven(number) ? 'yes' : 'no';
+  return { question, correctAnswer: answer };
+};
+
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export default () => {
-  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let i = 0;
-  while (i <= 2) {
-    const randomNumb = Math.round(Math.random() * 100);
-    const question = `Question: ${randomNumb}`;
-    console.log(question);
-    const answer = readlineSync.question('Your answer: ');
-    if ((randomNumb % 2 === 0 && answer === 'yes') || (randomNumb % 2 !== 0 && answer === 'no')) {
-      console.log('Correct!');
-    } else if ((randomNumb % 2 !== 0 && answer === 'yes')) {
-      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
-      return console.log(`Let's try again, ${name}!`);
-    } else if (randomNumb % 2 === 0 && answer === 'no') {
-      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-      return console.log(`Let's try again, ${name}!`);
-    } else {
-      return console.log('You may type only \'yes\' or \'no\'. Please, try again!');
-    }
-    i += 1;
-
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  }
-  return null;
+  console.log('Welcome to the Brain Games!');
+  console.log(gameDescription);
+  runGame(generateQuestion, gameDescription, name);
 };
--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import readlineSync from 'readline-sync';
 
 export const greeting = () => {
   console.log('Welcome to the Brain Games!');
-
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 
   return name;
 };
 
-export default greeting;
+export const runGame = (generateQuestion, gameDescription, playerName) => {
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i+=1) {
+    const { question, correctAnswer } = generateQuestion();
+    console.log(question);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
+};
+
