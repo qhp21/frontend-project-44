4160b5f59c5c615e2e55768f78769c47ecb8b049
Михаил Вебер <veber_22@bk.ru>
2023-05-18 23:29:17 +0300

    run calc

--- src/games/cal.js
+++ src/games/cal.js
@@ -1,48 +1,36 @@
 import readlineSync from 'readline-sync';
-
-import { greeting } from '../index.js';
+import { runGame } from '../index.js';
 
 export const calcCond = (answer, subtract, name) => console.log(`${answer} is wrong answer ;(. Correct answer was ${subtract}.\nLet's try again, ${name}!`);
-export default () => {
-  const name = greeting();
-  console.log('What is the result of the expression?');
 
-  let i = 0;
+const generateQuestion = () => {
+  const generator1 = Math.floor(Math.random() * 100);
+  const generator2 = Math.floor(Math.random() * 10);
+  const signsArr = ['+', '-', '*'];
+  const signsArrRand = Math.floor(Math.random() * signsArr.length);
+  const summ = generator1 + generator2;
+  const subtract = generator1 - generator2;
+  const mult = generator1 * generator2;
+  const question = `Question: ${generator1} ${signsArr[signsArrRand]} ${generator2} `;
+  let correctAnswer;
 
-  while (i <= 2) {
-    const generator1 = Math.floor(Math.random() * 100);
-    const generator2 = Math.floor(Math.random() * 10);
-    const signsArr = ['+', '-', '*'];
-    const signsArrRand = Math.floor(Math.random() * signsArr.length);
-    const summ = generator1 + generator2;
-    const subtract = generator1 - generator2;
-    const mult = generator1 * generator2;
-    const question = `Question: ${generator1} ${signsArr[signsArrRand]} ${generator2} `;
-    console.log(question);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (((question[13] || question[12]) === '+') && answer === summ.toString()) {
-      console.log('Correct!');
-    } else if (((question[13] || question[12]) === '-') && answer === subtract.toString()) {
-      console.log('Correct!');
-    } else if (((question[13] || question[12]) === '*') && answer === mult.toString()) {
-      console.log('Correct!');
-    }
-    if (answer === '') {
-      return console.log(`${answer} is wrong answer ;(. Correct answer was ${subtract}.\nLet's try again, ${name}!`);
-    } if ((answer !== summ.toString()) && ((question[13] || question[12]) === '+')) {
-      return calcCond(answer, summ, name);
-    } if ((answer !== subtract.toString()) && ((question[13] || question[12]) === '-')) {
-      return calcCond(answer, subtract, name);
-    } if ((answer !== mult.toString()) && ((question[13] || question[12]) === '*')) {
-      return calcCond(answer, mult, name);
-    }
-
-    i += 1;
-
-    if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
-    }
+  if (signsArr[signsArrRand] === '+') {
+    correctAnswer = summ.toString();
+  } else if (signsArr[signsArrRand] === '-') {
+    correctAnswer = subtract.toString();
+  } else if (signsArr[signsArrRand] === '*') {
+    correctAnswer = mult.toString();
   }
-  return null;
+
+  return { question, correctAnswer };
+};
+
+const gameDescription = 'What is the result of the expression?';
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Welcome to the Brain Games!');
+  console.log(gameDescription);
+  runGame(generateQuestion, gameDescription, name);
 };
