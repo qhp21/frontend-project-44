a35409014287790a1ad78671edc2bf712d340273
qhp21 <veber_33@bk.ru>
2023-05-19 12:09:17 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 import { runGame } from '../index.js';
 
-export const isPrime = (num) => {
+const isPrime = (num) => {
   for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
     if (num % i === 0) {
       return false;
@@ -22,7 +22,7 @@ const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer
 export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello! ${name}!`);
+  console.log(`Hello, ${name}!`);
   console.log(gameDescription);
   runGame(generateQuestion, gameDescription, name);
 };
